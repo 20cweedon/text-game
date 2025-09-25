af3142e73c35fecd644a478a3ea17889eb962243
20cweedon <20cweedon@thelangton.org.uk>
2025-09-25 15:53:39 +0200

    Update text_game.py
    Wey

--- text_game.py
+++ text_game.py
@@ -21,14 +21,10 @@ stats = ["HP 0","ATTACK 0","DEFENCE 0","SPEED 0", "INTELLIGENCE 0", "CHARISMA 0"
 
 name = input("Please enter your name adventurer")
 print ("please choose your class")
-ask 
+question
 print ("oh, hi there", name, "and welcome to Uravia")
 print ("we have been searching for a brave new adventurer such as yourself")
-ask = input("Will you help us? y/n")
-if ask == y:
-    print("thank you so much you kind soul")
-else:
-    print("fuck off you're helping anyway")
+ask = question("Will you help us? y/n", "y, n")
 print("I'm sending you to the centre of our continent where the strife is greatest, please save us all")
 
 ask = input("Grab, Use, Talk, Run, Attack")
